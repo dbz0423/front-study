d4e0aa555d471c5ec7d074bdff09187010e2d60b
dbz0423 <2569876934@qq.com>
2024-09-07 12:27:27 +0800

    second commit

--- TypeScript/引用类型.ts
+++ TypeScript/引用类型.ts
@@ -1,13 +1,13 @@
-let num1: number[] = [1,2,3]
-let num2: Array<number> = [4,5,6]
-let arr1: (number | string) [] = [1,2,'3','4']
-let arr2: any[] = [1,'2',true,null]
+//数组
+let num1:number[] =[1,2,3]
+let num2:Array<number>=[4,5,6]
+//多类型引用
+let arr1:(number|string)[]=[1,2,'3','4']
+let arr2:any[]=[1,'2',true,null]
 
-var date: object = new Date()
-var fun: object = () => {}
-var arr: object = ['object']
+//对象类型引用
+var date:object =new Date()   //引用一个对象
+var fun:object=()=>{}    //引用一个方法
+var arr:object=['object']   
 
-// var num:object = 2
-// 错误：不能将number类型分配给object类型
-// var str:object = 'hello world'
-// 错误：不能将string类型分配给object类型
+
